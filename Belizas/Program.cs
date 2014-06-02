2eef817bafc0c47f9bd6c75abd9e6ae64e29bd77
Rafael Escobar <rafael.bze@gmail.com>
2014-06-02 12:31:21 -0300

    Calculate the arguments, the negation and the logical and rawly.

---
+++ Belizas/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Nhanderu.Belizas
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            //Configures the console window.
+            Console.Title = "Belizas";
+
+            //Writes the very first informations and gets the formula.
+            String formula = "";
+            do
+            {
+                Console.WriteLine("Digite uma WFF para calcular a tabela ou \"?\" para ir às configurações:");
+                formula = Console.ReadLine().Replace(" ", "").ToLower();
+                Console.Clear();
+            }
+            while (String.IsNullOrWhiteSpace(formula));
+
+            //Verifies if the formula is valid.
+            if (TruthTable.HasDisallowedCharacters(formula, TruthTable.Expressions.Enumerate()))
+                Console.WriteLine("Fórmula inválida.");
+            else if (formula.StartsWith("?"))
+                Console.WriteLine("Menu de opções.");
+            else
+            {
+                //Writes the first lines of the reply.
+                Console.Title = "Carregando";
+                Console.WriteLine("Fórmula digitada:");
+                Console.WriteLine(formula + "\n");
+
+                #region Arguments
+                Console.WriteLine("Argumentos da tabela verdade:");
+
+                //Gets the arguments in formula
+                List<Char> arguments = new List<Char>();
+                foreach (Char item in formula.ToCharArray())
+                    if (Char.IsLetter(item) && !arguments.Contains(item))
+                        arguments.Add(item);
+
+                //Writes the first line of the truth table: the arguments.
+                foreach (Char argument in arguments)
+                    Console.Write(argument.ToString() + " ");
+                Console.WriteLine("");
+
+                //Writes the first columns: the arguments and its possible values.
+                Boolean[,] values = new Boolean[(Int32)Math.Pow(2, arguments.Count), arguments.Count];
+                for (Int32 line = 0; line < Math.Pow(2, arguments.Count); line++)
+                {
+                    Int32 calculableLine = line;
+                    for (Int32 column = 0; column < arguments.Count; column++)
+                    {
+                        if (calculableLine >= Math.Pow(2, (arguments.Count - 1) - column))
+                        {
+                            values[line, column] = true;
+                            calculableLine -= (Int32)Math.Pow(2, (arguments.Count - 1) - column);
+                        }
+                        else
+                            values[line, column] = false;
+                        Console.Write(Convert.ToInt32(values[line, column]).ToString() + " ");
+                    }
+                    Console.WriteLine("");
+                }
+                #endregion
+                Console.WriteLine("");
+
+                #region Negation
+                Console.WriteLine("Argumentos negados da tabela verdade:");
+
+                //Gets arguments in formula by the position of the negation symbol (').
+                List<Char> negationArguments = TruthTable.GetArgumentsInExpression(formula, arguments.Count);
+
+                //Verifies is there is at least one argument.
+                Boolean[,] negationValues = new Boolean[(Int32)Math.Pow(2, arguments.Count), negationArguments.Count];
+                if (negationArguments.Count > 0)
+                {
+                    //Writes the first line of the truth table: the expressions (negated arguments).
+                    List<Int32> indexesOfNegationArguments = new List<Int32>();
+                    foreach (Char negation in negationArguments)
+                    {
+                        Console.Write(negation.ToString() + "\' ");
+                        indexesOfNegationArguments.Add(arguments.IndexOf(negation));
+                    }
+                    Console.WriteLine("");
+
+                    //Writes the first columns: the expressions (negated arguments) and its values.
+                    for (int line = 0; line < Math.Pow(2, arguments.Count); line++)
+                    {
+                        for (int column = 0; column < negationArguments.Count; column++)
+                        {
+                            negationValues[line, column] = !values[line, indexesOfNegationArguments[column]];
+                            Console.Write(Convert.ToInt32(negationValues[line, column]).ToString() + "  ");
+                        }
+                        Console.WriteLine("");
+                    }
+                }
+                else
+                    Console.WriteLine("Não há argumentos negados.");
+                #endregion
+                Console.WriteLine("");
+
+                #region Logical and expressions
+                Console.WriteLine("Expressões de e lógico:");
+
+                //Gets arguments in formula by the position of the logical and symbol (.).
+                List<Char[]> andArguments = TruthTable.GetArgumentsInExpression(formula, arguments.Count, TruthTable.Expressions.And);
+
+                //Verifies is there is at least one argument.
+                Boolean[,] andValues = new Boolean[(Int32)Math.Pow(2, arguments.Count), andArguments.Count];
+                if (andArguments.Count > 0)
+                {
+                    //Writes the first line of the truth table: the expressions (negated arguments).
+                    List<Int32[]> indexesOfAndArguments = new List<Int32[]>();
+                    foreach (Char[] and in andArguments)
+                    {
+                        Console.Write(and[0].ToString() + TruthTable.Expressions.And + and[1].ToString());
+                        indexesOfAndArguments.Add(new Int32[] { arguments.IndexOf(and[0]), arguments.IndexOf(and[1]) });
+                    }
+                    Console.WriteLine("");
+
+                    //Writes the first columns: the expressions (negated arguments) and its values.
+                    for (int line = 0; line < Math.Pow(2, arguments.Count); line++)
+                    {
+                        for (int column = 0; column < andArguments.Count; column++)
+                        {
+                            andValues[line, column] = values[line, indexesOfAndArguments[column][0]] && values[line, indexesOfAndArguments[column][1]];
+                            Console.Write(" " + Convert.ToInt32(andValues[line, column]).ToString() + "  ");
+                        }
+                        Console.WriteLine("");
+                    }
+                }
+                else
+                    Console.WriteLine("Não há expressões de e lógico.");
+
+                #endregion
+                Console.WriteLine("");
+            }
+
+            Console.Title = "Belizas";
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Belizas/TruthTable.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nhanderu.Belizas
+{
+    public static class TruthTable
+    {
+        public static class Expressions
+        {
+            public static Char Negation = '\'',
+            And = '.',
+            Or = '+',
+            Xor = ':',
+            IfThen = '>',
+            ThenIf = '<',
+            IfAndOnlyIf = '-';
+        }
+
+        public static Char[] AllowedCharacters
+        {
+            get
+            {
+                return new Char[] { '\'', '.', '+', ':', '>', '<', '-' };
+            }
+        }
+
+        public static Boolean HasDisallowedCharacters(String text, Char[] rule)
+        {
+            List<Boolean> charactersStatus = new List<Boolean>();
+            Boolean isDisallowed = true, hasDisallowedCharacters = false;
+
+            foreach (Char character in text)
+            {
+                if (Char.IsLetter(character))
+                    isDisallowed = false;
+                else
+                    for (Int32 i = 0; i < rule.Length; i++)
+                        if (character == rule[i])
+                            isDisallowed = false;
+
+                charactersStatus.Add(isDisallowed);
+            }
+
+            foreach (Boolean status in charactersStatus)
+                if (status)
+                    hasDisallowedCharacters = true;
+
+            return hasDisallowedCharacters;
+        }
+
+        public static List<Char> GetArgumentsInExpression(String formula, Int32 quantityOfArguments)
+        {
+            List<Char> arguments = new List<Char>();
+
+            if (formula.IndexOf(Expressions.Negation) >= 0)
+            {
+                List<Int32> symbolPositions = new List<Int32>();
+                symbolPositions.Add(formula.IndexOf(Expressions.Negation));
+
+                for (Int32 i = 1; i < quantityOfArguments; i++)
+                    if (formula.IndexOf(Expressions.Negation, symbolPositions[symbolPositions.Count - 1] + 1) > 0)
+                        symbolPositions.Add(formula.IndexOf(Expressions.Negation, symbolPositions[symbolPositions.Count - 1] + 1));
+
+                foreach (Int32 symbolPosition in symbolPositions)
+                    arguments.Add(formula.ToCharArray()[symbolPosition - 1]);
+            }
+
+            return arguments;
+        }
+        public static List<Char[]> GetArgumentsInExpression(String formula, Int32 quantityOfArguments, Char expressionOperator)
+        {
+            List<Char[]> arguments = new List<Char[]>();
+
+            if (formula.IndexOf(expressionOperator) >= 0)
+            {
+                List<Int32> symbolPositions = new List<Int32>();
+                symbolPositions.Add(formula.IndexOf(expressionOperator));
+
+                for (Int32 i = 1; i < quantityOfArguments; i++)
+                    if (formula.IndexOf(expressionOperator, symbolPositions[symbolPositions.Count - 1] + 1) > 0)
+                        symbolPositions.Add(formula.IndexOf(expressionOperator, symbolPositions[symbolPositions.Count - 1] + 1));
+
+                foreach (Int32 symbolPosition in symbolPositions)
+                    arguments.Add(new Char[] { formula.ToCharArray()[symbolPosition - 1], formula.ToCharArray()[symbolPosition + 1] });
+            }
+
+            return arguments;
+        }
+    }
+}

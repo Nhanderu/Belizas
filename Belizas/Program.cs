aec15f460944741a9c03ddad58402079b14ee941
Rafael Escobar <rafael.bze@gmail.com>
2014-06-18 14:30:43 -0300

    Change the treatment of the exception.

--- Belizas/Program.cs
+++ Belizas/Program.cs
@@ -24,25 +24,26 @@ namespace Nhanderu.Belizas
             if (formula.StartsWith("?"))
                 Console.WriteLine("Menu de opções.");
             else if (!table.ValidateFormula())
-                Console.WriteLine("Fórmula inválida.");
+                Console.WriteLine("Fórmula inválida.\n");
             else
             {
                 Console.Title = "Carregando";
                 Console.WriteLine("Fórmula digitada:");
                 Console.WriteLine(formula + "\n");
 
-                Exception error = null;
                 try
                 {
-                    Boolean[,] test = new Boolean[(Int32)Math.Pow(2, table.Arguments.Count), table.Arguments.Count];
-                    test = null;
+                    table.CalculateArguments();
+                    table.CalculateExpressions();
+
+                    Console.WriteLine("Tabela gerada:");
+                    Console.WriteLine(table.ToString());
                 }
-                catch (OutOfMemoryException exception)
+                catch (OutOfMemoryException)
                 {
                     Console.Clear();
                     Console.WriteLine("ERRO");
-                    Console.WriteLine("Sua memória é um lixo e não aguenta a quantidade de valores que a tabela resultante possui.");
-                    Console.WriteLine("");
+                    Console.WriteLine("A tabela resultante ultrapassa o limite de memória reservada para este programa.");
                     Console.WriteLine("Os argumentos vão gerar uma matriz de {0}, que é a quantidade de argumentos, por {1}, que é 2 elevado à quantidade de argumentos.", table.Arguments.Count, Math.Pow(2, table.Arguments.Count));
                     Console.WriteLine("Logo, o total de valores na tabela vai ser de {0} x {1} = {2} bytes.", table.Arguments.Count, Math.Pow(2, table.Arguments.Count), (table.Arguments.Count * Math.Pow(2, table.Arguments.Count)));
 
@@ -60,17 +61,6 @@ namespace Nhanderu.Belizas
                     Console.WriteLine("");
                     Console.WriteLine("Escreva fórmulas menores da próxima vez.");
                     Console.WriteLine("");
-
-                    error = exception;
-                }
-
-                if (error == null)
-                {
-                    table.CalculateArguments();
-                    table.CalculateExpressions();
-
-                    Console.WriteLine("Tabela gerada:");
-                    Console.WriteLine(table.ToString());
                 }
             }
 

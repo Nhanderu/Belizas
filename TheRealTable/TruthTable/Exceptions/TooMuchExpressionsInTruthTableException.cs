76767408e443e276539adaa31520fc36b7e77f96
Rafael Escobar <rafael.bze@gmail.com>
2014-12-15 17:13:27 -0200

    Implement "too much expressions" exception.

--- TheRealTable/TruthTable/Exceptions/TooMuchExpressionsInTruthTableException.cs
+++ TheRealTable/TruthTable/Exceptions/TooMuchExpressionsInTruthTableException.cs
@@ -4,5 +4,6 @@ namespace Nhanderu.TheRealTable.TruthTable.Exceptions
 {
     class TooMuchExpressionsInTruthTableException : TooMuchInformationInTruthTableException
     {
+        public TooMuchExpressionsInTruthTableException() : base("There's a lot of expressions in the formula, which is more than the memory of this computer can handle or than the OS has reserved for this program.") { }
     }
 }

c25f224fea711567d5c94e8c3bae5790de79e5f0
Rafael Escobar <rafael.bze@gmail.com>
2014-12-18 19:36:59 -0200

    Test ValidateFormula.

--- TheRealTable.Test/TruthTableTest.cs
+++ TheRealTable.Test/TruthTableTest.cs
@@ -8,6 +8,28 @@ namespace Nhanderu.TheRealTable.Test
     [TestClass]
     public class TruthTableTest
     {
+        TruthTable _exampleTable = new TruthTable("");
+
+        #region Constructor
+        [TestMethod]
+        [ExpectedException(typeof(InvalidFormulaException))]
+        public void Constructor_EmptyStringAsFormula_InvalidFormulaExceptionThrown()
+        {
+            TruthTable table = new TruthTable("", true);
+        }
+        #endregion
+
+        #region Calculate
+        [TestMethod]
+        [ExpectedException(typeof(InvalidFormulaException))]
+        public void Calculate_EmptyStringAsFormula_InvalidFormulaExceptionThrown()
+        {
+            TruthTable table = new TruthTable("");
+            table.Calculate();
+        }
+        #endregion
+
+        #region Formula
         [TestMethod]
         [ExpectedException(typeof(InvalidFormulaException))]
         public void Formula_EmptyStringAsFormula_InvalidFormulaExceptionThrown()
@@ -15,12 +37,37 @@ namespace Nhanderu.TheRealTable.Test
             TruthTable table = new TruthTable("");
             table.Formula = "";
         }
+        #endregion
+
+        #region ValidateFormula
+        [TestMethod]
+        public void ValidateFormula_EmptyStringAsFormula_InvalidFormula()
+        {
+            Assert.AreEqual(false, _exampleTable.ValidateFormula(""));
+        }
+        [TestMethod]
+        public void ValidateFormula_FormulaWithUnclosedBrackets_InvalidFormula()
+        {
+            Assert.AreEqual(false, _exampleTable.ValidateFormula("a.(b.(c.(d.(e.f)"));
+        }
 
         [TestMethod]
-        [ExpectedException(typeof(InvalidFormulaException))]
-        public void Formula_EmptyStringAsFormulaInConstructor_InvalidFormulaExceptionThrown()
+        public void ValidateFormula_FormulaWithFollowingOperators_InvalidFormula()
         {
-            TruthTable table = new TruthTable("", true);
+            Assert.AreEqual(false, _exampleTable.ValidateFormula("a.b+c>d<f--g"));
         }
+
+        [TestMethod]
+        public void ValidateFormula_FormulaEndingWithOperator_InvalidFormula()
+        {
+            Assert.AreEqual(false, _exampleTable.ValidateFormula("a.b+c>d<f-"));
+        }
+
+        [TestMethod]
+        public void ValidateFormula_FormulaWithDisallowedCharacter_InvalidFormula()
+        {
+            Assert.AreEqual(false, _exampleTable.ValidateFormula("a.b+c>d<f?g"));
+        }
+        #endregion
     }
 }

7e25d213d3a5be1916f8000be2cf12376e22090d
Rafael Escobar <rafael.bze@gmail.com>
2015-03-06 17:35:10 -0300

    Add a demo project for testing.

---
+++ Belizas.Demo/Program.cs
@@ -0,0 +1,30 @@
+﻿using Nhanderu.Belizas;
+using System;
+using System.Collections.Generic;
+
+namespace Belizas.Demo
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            String formula;
+            TruthTable table = new TruthTable("");
+            do
+            {
+                formula = Console.ReadLine();
+            } while (!table.ValidateFormula(formula));
+
+            table = new TruthTable(formula, true);
+
+            Console.Write(table.ToString());
+            Console.ReadKey();
+            Console.WriteLine(table.ToHtmlTable(new Dictionary<String, Object>() { { "foo", "bar" }, { "nhan", "deru" } }, trAttributes: new Dictionary<String, Object>() { { "foo", "bar" }, { "nhan", "deru" } }));
+            Console.ReadKey();
+            Console.WriteLine(table.ToHtmlTable(new { foo = "bar", nhan = "deru", ra_fa = "el" }));
+            Console.ReadKey();
+            Console.WriteLine(table.ToHtmlTable());
+            Console.ReadKey();
+        }
+    }
+}

e93c79d9763b1497f223ef28f6753952d43f81ce
Rafael Escobar <rafael.bze@gmail.com>
2015-03-09 20:35:31 -0300

    Update assembly informations file.

--- Belizas/Properties/AssemblyInfo.cs
+++ Belizas/Properties/AssemblyInfo.cs
@@ -1,36 +1,24 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("TheRealTable")]
-[assembly: AssemblyDescription("")]
+// General Information about an assembly is controlled through the following set of attributes.
+[assembly: AssemblyTitle("Belizas")]
+[assembly: AssemblyDescription("A simple library for truth table calculus")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TheRealTable")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Nhanderu")]
+[assembly: AssemblyProduct("Belizas")]
+[assembly: AssemblyCopyright("Copyright © 2014-2015 Rafael Escobar @ nhanderu.com")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture("en-US")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+// Setting ComVisible to false makes the types in this assembly not visible to COM components.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
+// The following GUID is for the ID of the typelib if this project is exposed to COM.
 [assembly: Guid("b71a8d69-8d74-43d7-9535-910751c07c80")]
 
 // Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+// Major Version, Minor Version, Build Number and Revision.
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]

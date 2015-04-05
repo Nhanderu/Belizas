9c2982eebd634c41fc3c7fc3b102d4c5c5767985
Rafael Escobar <rafael.bze@gmail.com>
2015-04-05 14:44:08 -0300

    Use one instance of TruthTable in tests.

--- Belizas.Test/TruthTableTest.cs
+++ Belizas.Test/TruthTableTest.cs
@@ -7,6 +7,8 @@ namespace Nhanderu.Belizas.Test
     [TestClass]
     public class TruthTableTest
     {
+        TruthTable _table = new TruthTable("");
+
         #region Properties
         [TestMethod]
         [ExpectedException(typeof(TableNotCalculatedException))]
@@ -46,88 +48,93 @@ namespace Nhanderu.Belizas.Test
         [ExpectedException(typeof(InvalidFormulaException))]
         public void Calculate_EmptyStringAsFormula_InvalidFormulaExceptionThrown()
         {
-            TruthTable table = new TruthTable("");
-            table.Calculate();
+            _table.Calculate();
         }
 
         [TestMethod]
         public void Calculate_LowerAndUpperCaseArguments_ShouldCountAsDifferentArguments()
         {
-            TruthTable table = new TruthTable("a.A.b");
+            _table.Formula = "a.A.b";
 
-            table.Calculate();
-            Assert.AreEqual(3, table.Arguments.Count);
+            _table.Calculate();
+            Assert.AreEqual(3, _table.Arguments.Count);
+
+            _table.Formula = "";
         }
         #endregion
 
         #region ValidateFormula
+        // Invalid.
         [TestMethod]
-        public void ValidateFormula_FormulaIsEmpty_InvalidFormula()
+        public void ValidateFormula_EmptyFormula_InvalidFormula()
         {
-            TruthTable table = new TruthTable("");
-
-            Assert.AreEqual(false, table.ValidateFormula(""));
-            Assert.AreEqual(false, table.ValidateFormula("              "));
+            Assert.AreEqual(false, _table.ValidateFormula(""));
+            Assert.AreEqual(false, _table.ValidateFormula("              "));
         }
+
         [TestMethod]
         public void ValidateFormula_FormulaWithUnclosedBrackets_InvalidFormula()
         {
-            TruthTable table = new TruthTable("");
-
-            Assert.AreEqual(false, table.ValidateFormula("a.(b.(c.(d.(e.f)"));
+            Assert.AreEqual(false, _table.ValidateFormula("a.(b.(c.(d.(e.f)"));
         }
 
         [TestMethod]
         public void ValidateFormula_FormulaWithFollowingOperators_InvalidFormula()
         {
-            TruthTable table = new TruthTable("");
-
-            Assert.AreEqual(false, table.ValidateFormula("a.b+c>d<f--g"));
-            Assert.AreEqual(false, table.ValidateFormula("a.a+a:a>a<a-a'.a+a:a>a<a-a'.a+a:a>>a<a-a'"));
+            Assert.AreEqual(false, _table.ValidateFormula("a.b+c>d<f--g"));
+            Assert.AreEqual(false, _table.ValidateFormula("a.a+a:a>a<a-a'.a+a:a>a<a-a'.a+a:a>>a<a-a'"));
         }
 
         [TestMethod]
         public void ValidateFormula_FormulaStartingAndEndingWithOperator_InvalidFormula()
         {
-            TruthTable table = new TruthTable("");
-
-            Assert.AreEqual(false, table.ValidateFormula("a.b+c>d<f-"));
-            Assert.AreEqual(false, table.ValidateFormula(".b+c>d<f-a"));
-            Assert.AreEqual(false, table.ValidateFormula(".b+c>d<f-"));
+            Assert.AreEqual(false, _table.ValidateFormula("a.b+c>d<f-"));
+            Assert.AreEqual(false, _table.ValidateFormula(".b+c>d<f-a"));
+            Assert.AreEqual(false, _table.ValidateFormula(".b+c>d<f-"));
         }
 
         [TestMethod]
         public void ValidateFormula_FormulaWithDisallowedCharacter_InvalidFormula()
         {
-            TruthTable table = new TruthTable("");
-
-            Assert.AreEqual(false, table.ValidateFormula("a.b+c>d<f?g"));
+            Assert.AreEqual(false, _table.ValidateFormula("a.b+c>d<f?g"));
         }
 
         [TestMethod]
         public void ValidateFormula_FormulaWithBracketsOnly_InvalidFormula()
         {
-            TruthTable table = new TruthTable("");
+            Assert.AreEqual(false, _table.ValidateFormula("()"));
+            Assert.AreEqual(false, _table.ValidateFormula("((((()))))"));
+            Assert.AreEqual(false, _table.ValidateFormula("((((()()()()))))"));
+        }
 
-            Assert.AreEqual(false, table.ValidateFormula("()"));
-            Assert.AreEqual(false, table.ValidateFormula("((((()))))"));
-            Assert.AreEqual(false, table.ValidateFormula("((((()()()()))))"));
+        // Valid.
+        [TestMethod]
+        public void ValidateFormula_JustAnArgument_ValidFormula()
+        {
+            Assert.AreEqual(true, _table.ValidateFormula("d"));
+        }
+
+        [TestMethod]
+        public void ValidateFormula_JustANegatedArgument_ValidFormula()
+        {
+            Assert.AreEqual(true, _table.ValidateFormula("d'"));
         }
 
         [TestMethod]
         public void ValidateFormula_SimpleFormula_ValidFormula()
         {
-            TruthTable table = new TruthTable("");
-
-            Assert.AreEqual(true, table.ValidateFormula("a.b"));
+            Assert.AreEqual(true, _table.ValidateFormula("a.b"));
+            Assert.AreEqual(true, _table.ValidateFormula("a+b"));
+            Assert.AreEqual(true, _table.ValidateFormula("a:b"));
+            Assert.AreEqual(true, _table.ValidateFormula("a>b"));
+            Assert.AreEqual(true, _table.ValidateFormula("a<b"));
+            Assert.AreEqual(true, _table.ValidateFormula("a-b"));
         }
 
         [TestMethod]
         public void ValidateFormula_ComplexFormula_ValidFormula()
         {
-            TruthTable table = new TruthTable("");
-
-            Assert.AreEqual(true, table.ValidateFormula("a+(b.c>(a'+b')'<b':((c-c')-a))'"));
+            Assert.AreEqual(true, _table.ValidateFormula("a+(b.c>(a'+b')'<b':((c-c')-a))'"));
         }
         #endregion
     }
